4b5d540be9ed427b8932e8fc52a811ece0e7e7d0
Dexter Krzeminski <dexter.krzeminski@gmail.com>
2024-10-24 17:58:35 +0200

    Added run animation and sprite flipping

--- Assets/Scripts/Mushroom/Mushroom.cs
+++ Assets/Scripts/Mushroom/Mushroom.cs
@@ -6,20 +6,46 @@ public class Mushroom : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] GameObject laternToFollow;
-
+    [SerializeField] float powerUpLevel = 0.00f;
+    [SerializeField] float powerLevelSpeed = 0.5f; // for prototype, move it to scriptobject later
     MushroomMovement mushroomMovement;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+
+    bool isMoving;
     void Awake()
     {
+        isMoving = false;
         mushroomMovement = GetComponent<MushroomMovement>();
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!mushroomMovement.IsClose(laternToFollow.transform.position))
+        isMoving = !mushroomMovement.IsClose(laternToFollow.transform.position);
+        if (isMoving)
         {
             mushroomMovement.Move(laternToFollow.transform.position);    
         }
-        
+        else
+        {
+            Rest();
+        }
+        //TODO move it to different script
+        FlipSprite();
+        animator.SetBool("Moving",isMoving);
+    }
+
+    private void FlipSprite()
+    {
+        spriteRenderer.flipX = (mushroomMovement.directionX == 1) ? true : false;
+    }
+
+    private void Rest()
+    {
+        powerUpLevel += powerLevelSpeed * Time.deltaTime;
+        //More later
     }
 }
--- Assets/Scripts/Mushroom/MushroomMovement.cs
+++ Assets/Scripts/Mushroom/MushroomMovement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class MushroomMovement : MonoBehaviour
@@ -9,10 +10,14 @@ public class MushroomMovement : MonoBehaviour
     [SerializeField] float moveSpeed = 2f;
     [SerializeField] float stopDistance = 2f;
 
+    public float directionX {get;set;}
+
     public void Move(Vector3 destinationPos)
     {
         Vector3 moveDir = destinationPos - transform.position;
         moveDir = moveDir.normalized;
+        directionX = Mathf.Sign(moveDir.x);
+        Debug.Log(moveDir);
         if (!IsClose(destinationPos))
         {
             transform.Translate(moveDir * moveSpeed * Time.deltaTime);
