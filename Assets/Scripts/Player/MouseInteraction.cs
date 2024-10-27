e1773129982eb1465409f6f2e7d1b04678eeefaa
Dexter Krzeminski <dexter.krzeminski@gmail.com>
2024-10-27 18:41:21 +0100

    Rename: ObjectDetection -> MouseInteraction

--- Assets/Scripts/Player/MouseInteraction.cs
+++ Assets/Scripts/Player/MouseInteraction.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class ObjectDetection : MonoBehaviour
+public class MouseInteraction : MonoBehaviour
 {
 
     Mouse mouse;
